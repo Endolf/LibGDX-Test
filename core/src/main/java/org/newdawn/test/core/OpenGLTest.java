99d309d3d16bfdcd950b6a8486c79d5c78eb60dc
Endolf <endolf@computerbooth.com>
2013-05-02 15:50:35 +0100

    Rotate the camera around with mouse/touch.

--- core/src/main/java/org/newdawn/test/core/OpenGLTest.java
+++ core/src/main/java/org/newdawn/test/core/OpenGLTest.java
@@ -5,15 +5,16 @@ import java.util.List;
 
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 
-public class OpenGLTest implements ApplicationListener {
+public class OpenGLTest implements ApplicationListener, InputProcessor {
 	
 	private Camera camera;
+	private float cameraXRot = -45,cameraYRot = 45;
 	private Cube cube;
 	private float runTime;
 	private List<Renderable> renderables = new ArrayList<Renderable>();
@@ -26,6 +27,12 @@ public class OpenGLTest implements ApplicationListener {
 		
 		renderables.add(cube);
 		renderables.add(new Cube());
+		
+		camera = new PerspectiveCamera(67, 1, 1);
+		camera.near = 3;
+		camera.far = 3000;
+		
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
@@ -35,9 +42,28 @@ public class OpenGLTest implements ApplicationListener {
 		Gdx.gl20.glEnable(GL20.GL_DEPTH_TEST);
 		Gdx.gl20.glDepthFunc(GL20.GL_LESS);
 		
-		camera = new PerspectiveCamera(67, width, height);
-		camera.translate(10,10,10);
-		camera.lookAt(0,0,0);
+		camera.viewportWidth = width;
+		camera.viewportHeight = height;
+		
+		rotateCamera(0,0);
+	}
+
+	private void rotateCamera(float xRot, float yRot) {
+		cameraXRot += xRot;
+		cameraYRot += yRot;
+		
+		if(cameraYRot>180) cameraYRot = cameraYRot - 360;
+		if(cameraYRot < -180) cameraYRot = cameraYRot + 360;
+		
+		if(cameraXRot > 90) cameraXRot = 90;
+		if(cameraXRot < -90) cameraXRot = -90;
+		
+		camera.position.set(0,0,5);
+		camera.position.rotate(cameraXRot, 1, 0, 0);
+		camera.position.rotate(cameraYRot, 0, 1, 0);
+		
+		camera.lookAt(0, 0, 0);
+
 		camera.update();
 	}
 
@@ -66,5 +92,54 @@ public class OpenGLTest implements ApplicationListener {
 
 	@Override
 	public void dispose () {
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		Gdx.app.debug("touch", "keyDown: " + keycode);
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		Gdx.app.debug("touch", "keyUp: " + keycode);
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		Gdx.app.debug("touch", "keyTyped: \"" + character + "\"");
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		Gdx.app.debug("touch", "touchDown: " + pointer + ", " + button);
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		Gdx.app.debug("touch", "touchUp: " + pointer + ", " + button);
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		Gdx.app.debug("touch", "touchDragged: " + pointer + ", x: " + Gdx.app.getInput().getDeltaX(pointer) + ",y: " + Gdx.app.getInput().getDeltaY(pointer));
+		rotateCamera(-Gdx.app.getInput().getDeltaY(pointer),-Gdx.app.getInput().getDeltaX(pointer));
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+//		Gdx.app.debug("touch", "mouseMoved: ");
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		Gdx.app.debug("touch", "scrolled: ");
+		return false;
 	}
 }
