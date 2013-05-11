06e11707dfe8936c979b72156220e87ecc0bb3d0
Endolf <endolf@computerbooth.com>
2013-05-11 17:27:09 +0100

    Add normals

--- core/src/main/java/org/newdawn/test/core/Triangle.java
+++ core/src/main/java/org/newdawn/test/core/Triangle.java
@@ -2,8 +2,20 @@ package org.newdawn.test.core;
 
 public class Triangle extends Shape {
 	public Triangle(boolean isBillboard) {
-		super(new float[] {0.0f,  0.622008459f, 0.0f,
-			-0.5f, -0.311004243f, 0.0f,
-			0.5f, -0.311004243f, 0.0f }, new short[] { 0, 1, 2 }, true, new float[]{0,1,0,1}, isBillboard);
+		super();
+		
+		setVertices(new float[] {0.0f,  0.622008459f, 0.0f,
+				-0.5f, -0.311004243f, 0.0f,
+				0.5f, -0.311004243f, 0.0f });
+		
+		setIndecies(new short[] { 0, 1, 2 });
+		setTwoSided(true);
+		setColour(new float[]{0,1,0,1});
+		setBillboard(isBillboard);
+		setNormals(new float[] {0,0,1,
+				0,0,1,
+				0,0,1});
+		
+		initialise();
 	}
 }
