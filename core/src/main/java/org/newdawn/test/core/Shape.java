2331066c258d9ef5ea3ed2af79c8e935f808ec9e
Endolf <endolf@computerbooth.com>
2013-05-11 17:26:59 +0100

    Support normals

--- core/src/main/java/org/newdawn/test/core/Shape.java
+++ core/src/main/java/org/newdawn/test/core/Shape.java
@@ -24,6 +24,10 @@ public abstract class Shape implements Renderable {
 		"attribute vec2 aTexCoord0;\n" +
 		"varying vec2 vTexCoord0;\n" +
 		"#endif\n" +
+		"#ifdef NORMALS\n" +
+		"attribute vec3 aNormal;\n" +
+		"varying vec3 vNormal;\n" +
+		"#endif\n" +
 		"void main() {\n" +
 		"#ifdef BILLBOARD\n" +
 		"  gl_Position = uProjectionMatrix * (uViewMatrix * uModelMatrix * vec4(0.0, 0.0, 0.0, 1.0) + vec4(aPosition.x, aPosition.y, 0.0, 0.0));\n" +
@@ -32,6 +36,9 @@ public abstract class Shape implements Renderable {
 		"#endif\n" +
 		"#ifdef TEXTURED\n" +
 		"  vTexCoord0 = aTexCoord0;\n" +
+		"#endif\n" +
+		"#ifdef NORMALS\n" +
+		"  vNormal = aNormal;\n" +
 		"#endif\n" +
 		"}\n";
 
@@ -44,12 +51,17 @@ public abstract class Shape implements Renderable {
 		"uniform sampler2D uTexture0;\n" +
 		"varying vec2 vTexCoord0;\n" +	
 		"#endif\n" +
+		"#ifdef NORMALS\n" +
+		"varying vec3 vNormal;\n" +
+		"#endif\n" +
 		"void main() {\n" + 
-		"#ifdef TEXTURED\n" +
-		"  gl_FragColor = texture2D(uTexture0, vTexCoord0) * uColor;\n" +
-		"#else\n" + 
 		"  gl_FragColor = uColor;\n" +
+		"#ifdef TEXTURED\n" +
+		"  gl_FragColor = texture2D(uTexture0, vTexCoord0) * gl_FragColor;\n" +
 		"#endif\n" + 
+		"#ifdef NORMALS\n" +
+		"  gl_FragColor = gl_FragColor + vec4(vNormal, 1);\n" +
+		"#endif\n" +
 		"}\n";
 
 	private ShaderProgram shader;
@@ -59,14 +71,16 @@ public abstract class Shape implements Renderable {
 	private float[] rotation = {0,0,1,0};
 
 	private float[] colour;
-	private Texture texture;
 	private float[] vertices;
 	private short[] indecies;
+	private float[] textureCoords;
+	private float[] normals;
+	private Texture texture;
+	
 	private boolean isTwoSided;
 	private boolean isBillboard;
 	private String textureLocation;
-	private float[] textureCoords;
-	
+
 	public Shape(float[] vertices, short[] indecies, boolean twoSided, float[] colour, boolean isBillboard) {
 		this.vertices = vertices;
 		this.indecies = indecies;
@@ -89,20 +103,25 @@ public abstract class Shape implements Renderable {
 		initialise();
 	}
 	
-	private void initialise() {
+	public Shape() {
+	}
+	
+	public void initialise() {
 		dispose();
 		
 		int numVertices = vertices.length / 3;
 		
 		int numDataTypes = 1;
 		if(textureCoords!=null) numDataTypes +=1;
+		if(normals!=null) numDataTypes +=1;
 		int numDataItems = 3;
 		if(textureCoords!=null) numDataItems+=2;
+		if(normals!=null) numDataItems+=3;
 		
 		float[] allVertexData = new float[numVertices * numDataItems];
 		
+		int dataItem = 0;
 		for(int i=0;i<numVertices;i++) {
-			int dataItem = i*numDataItems;
 			allVertexData[dataItem++] = vertices[i*3];
 			allVertexData[dataItem++] = vertices[i*3+1];
 			allVertexData[dataItem++] = vertices[i*3+2];
@@ -111,12 +130,19 @@ public abstract class Shape implements Renderable {
 				allVertexData[dataItem++] = textureCoords[i*2];
 				allVertexData[dataItem++] = textureCoords[i*2+1];
 			}
+			
+			if(normals!=null) {
+				allVertexData[dataItem++] = normals[i*3];
+				allVertexData[dataItem++] = normals[i*3+1];
+				allVertexData[dataItem++] = normals[i*3+2];
+			}
 		}
 		
 		VertexAttribute[] vAttribs = new VertexAttribute[numDataTypes];
-		int dataItem = 0;
+		dataItem = 0;
 		vAttribs[dataItem++] = new VertexAttribute(Usage.Position, 3, "aPosition");
 		if(textureCoords!=null) vAttribs[dataItem++] = new VertexAttribute(Usage.TextureCoordinates, 2, "aTexCoord0");
+		if(normals!=null) vAttribs[dataItem++] = new VertexAttribute(Usage.Normal, 3, "aNormal");
 		
 		mesh = new Mesh(true, numVertices, indecies.length, vAttribs);
 
@@ -139,6 +165,10 @@ public abstract class Shape implements Renderable {
 		if(isBillboard) {
 			finalVertexShaderCode += "#define BILLBOARD\n";
 			finalFragmentShaderCode += "#define BILLBOARD\n";
+		}
+		if(normals!=null) {
+			finalVertexShaderCode += "#define NORMALS\n";
+			finalFragmentShaderCode += "#define NORMALS\n";
 		}
 		
 		finalVertexShaderCode += vertexShaderCode;
@@ -212,5 +242,69 @@ public abstract class Shape implements Renderable {
 		if(shader!=null) shader.dispose();
 		if(texture!=null) texture.dispose();
 		if(mesh!=null) mesh.dispose();
+	}
+
+	public float[] getColour() {
+		return colour;
+	}
+
+	public void setColour(float[] colour) {
+		this.colour = colour;
+	}
+
+	public float[] getVertices() {
+		return vertices;
+	}
+
+	public void setVertices(float[] vertices) {
+		this.vertices = vertices;
+	}
+
+	public short[] getIndecies() {
+		return indecies;
+	}
+
+	public void setIndecies(short[] indecies) {
+		this.indecies = indecies;
+	}
+
+	public float[] getTextureCoords() {
+		return textureCoords;
+	}
+
+	public void setTextureCoords(float[] textureCoords) {
+		this.textureCoords = textureCoords;
+	}
+
+	public float[] getNormals() {
+		return normals;
+	}
+
+	public void setNormals(float[] normals) {
+		this.normals = normals;
+	}
+
+	public boolean isTwoSided() {
+		return isTwoSided;
+	}
+
+	public void setTwoSided(boolean isTwoSided) {
+		this.isTwoSided = isTwoSided;
+	}
+
+	public boolean isBillboard() {
+		return isBillboard;
+	}
+
+	public void setBillboard(boolean isBillboard) {
+		this.isBillboard = isBillboard;
+	}
+
+	public String getTextureLocation() {
+		return textureLocation;
+	}
+
+	public void setTextureLocation(String textureLocation) {
+		this.textureLocation = textureLocation;
 	}
 }
